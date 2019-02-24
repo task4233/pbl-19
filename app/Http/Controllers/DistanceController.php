ffcf01fbcfb1bcdb835c6485ec44a3eefe1bee1c
task4233 <tks.m1205@gmail.com>
2019-02-24 07:53:07 +0000

    [ADD] some functions by using minh's code

--- app/Http/Controllers/DistanceController.php
+++ app/Http/Controllers/DistanceController.php
@@ -3,11 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Distance;
 
 class DistanceController extends Controller
 {
     public function index()
     {
+        $emp_addresses = Distance::getEmployeesAddresses();
+        $resingned_addresses = Distance::getResignedPeopleAddresses();
         return view('distance');
     }
+    
+    // get the Distance from (lat1, lon1) to (lat2, lon2)
+    private static function getDistanceFromLatLonInKm($lat1, $lon1, $lat2, $lon2) {
+        const R = 6371; // Radius of the earth in km
+        $dLat = deg2rad($lat2-$lat1);  // deg2rad below
+        $dLon = deg2rad($lon2-$lon1); 
+        $a = 
+           sin($dLat/2) * sin($dLat/2) +
+           cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * 
+           sin($dLon/2) * sin($dLon/2)
+           ; 
+        $c = 2 * atan2(sqrt($a), sqrt(1-$a)); 
+        $d = R * $c; // Distance in km
+        return $d;
+    }
+    
+    private static function deg2rad($deg) {
+        return $deg * (pi()/180);
+    }
+    
 }
--- app/Models/Distance.php
+++ app/Models/Distance.php
@@ -3,14 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Distance extends Model
 {
     protected $table = 'distances';
     protected $fillable = [];
 
-    public static function getDistances(){
-        
+    // get addresses of employees
+    public static function getEmployeesAddresses() {
+        $emp_addresses = DB::table('employees')
+                       ->whereNotNull('address')
+                       ->whereRaw('address != ""')
+                       ->groupBy('address')
+                       ->get();
     }
-    //
+
+    // get addresses of resigned people
+    public static function getResignedPeopleAddresses(){
+        $resigned_people_addresses = DB::table('leaves')
+                                   ->whereNotNull('address')
+                                   ->whereRaw('address != ""')
+                                   ->groupBy('address')
+                                   ->get();
+        return $resinged_people_addresses;
+    }
+
 }
