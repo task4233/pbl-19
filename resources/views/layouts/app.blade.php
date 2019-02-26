40c54819393294a105f7a17381fff38859778fb5
task4233 <tks.m1205@gmail.com>
2019-02-27 03:17:55 +0900

    Add heading conclusion (#44)
    * [ADD] heading conclusion in Reason_Types
    
    * [UPDATE] reason types page
    
    * [UPDATE] gender, marital status and age page
    
    * [UPDATE] position page
    
    * [UPDATE] distance page
    
    * [ADD] heading conclusion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,51 +1,90 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <title>@yield('title')</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.5.0"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Niconne" rel="stylesheet">
+<meta charset="utf-8">
+      <title>@yield('title')</title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+      <link href="https://fonts.googleapis.com/css?family=Niconne" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Lato:400,900" rel="stylesheet">
     <style type="text/css">
       html {
-          background-color: #E1E1E1;
           height: 100%;
       }
       body {
-          background-color: #E1E1E1;
+          font-family: 'Lato', 'Noto Sans JP', '游ゴシック Medium', '游ゴシック体', 'Yu Gothic Medium', YuGothic, 'ヒラギノ角ゴ ProN', 'Hiragino Kaku Gothic ProN', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック', 'MS PGothic', sans-serif;
           height: 100%;
           margin: auto;
-          text-align: center;
+					letter-spacing: .1em;
+					line-height: 1.65;
+					font-size: 30px;
       }
-    .navbar-brand {
-        font-family: 'Niconne', cursive;
-        font-size: 30px;
-     }
-     .content {
-         background-color: #E1E1E1;
-         height: 100%;
+		 h1 {
+				 text-align: center;
+		 }
+.navbar-brand {
+    font-family: 'Niconne', cursive;
+    font-size: 30px;
+ }
+.container {
+             margin: 5% auto;
+    background-color: #E1E1E1;
+             padding: 2%;
       }
       .chart {
+					background-color: #E1E1E1;
+					width: 85%;
           height: 80%;
           margin: 0 auto;
-       }
+					padding: 2%
+      }
+		  table {
+					background-color: white; 
+			}
+		 .conclusion {
+				 text-decoration: underline;
+		 }
+.center {
+    text-align: center;
+ }
+
+.hide {
+              display: none;
+ }
     </style>
 </head>
 
-<body class="body">
+<body>
     @include('layouts.nav')
     
-    <h1>@yield('title')</h1>
+    <h1 style="font-size:60px;">@yield('title')</h1>
 	
-    <div class="content">
+		<div class="container">
+				@yield('chart')
+		</div>
+
+		<div class="container">
+				<h1>Results</h1>
+				@yield('table')
+	  </div>
+
+		<div class="container">
+				<h1>Study from Chart</h1>
+				@yield('study')
+		</div>
+		
+		<div class="container">
+				<h1>Discuss @yield('title')</h1>
+				@yield('discuss')
+		</div>
+
+    <div class="container ">
+				<h1>Heading Conclusion</h1>
         @yield('content')
     </div>
-    <div class="footer">
-        @yield('footer')
-    </div>
+
     <!-- load scripts-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
--- resources/views/position.blade.php
+++ resources/views/position.blade.php
@@ -2,52 +2,65 @@
 
 @section('title', 'Position')
 
-@section('content')
-<div class="chart">
+@section('chart')
 		<!-- chart.js -->
-		<canvas id="positionChart"></canvas>
+		<canvas id="positionChart" height=400></canvas>
 		<!-- end -->
-</div>
 
 <script>
  var ctx = document.getElementById("positionChart");
  var positionChart = new Chart(ctx, {
      type: 'bar',
      data: {
-        labels: [
-            @foreach ($resigned_positions_short as $key => $value)
-                "{{ ucfirst($key) }}",
-            @endforeach
-        ],
-        
-        datasets: [{
-            borderColor: 'ghostwhite',
-            borderWidth: 1,
-            backgroundColor: [
-                @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
-                    "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 70%, 45%)",
-                @endfor
-            ],
-            data: [
-                @foreach ($emp_positions_short as $key => $value)
-                    {{ $value }},
-                @endforeach
-            ],
-        },
-        {
-            borderColor: 'gray',
-            borderWidth: 1,
-            backgroundColor: [
-                @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
-                    "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 50%, 40%)",
-                @endfor
-            ],
-            data: [
-                @foreach ($resigned_positions_short as $key => $value)
-                    {{ $value }},
-                @endforeach
-            ],
-         }],
+     // labels
+             labels: [
+                 @foreach ($resigned_positions_short as $key => $value)
+                 "{{ ucfirst($key) }}",
+                 @endforeach
+             ],
+             //dataset
+             datasets: [{
+                     label: 'Not resigned',
+                     borderColor: 'ghostwhite',
+                     borderWidth: 1,
+                     // bg-color
+                     backgroundColor: [
+                         @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
+                         "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 70%, 45%)",
+                         @endfor
+                     ],
+                     hoverBackgroundColor: [
+                          @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
+                         "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 80%, 45%)",
+                         @endfor
+                     ],
+                     data: [
+                         @foreach ($emp_positions_short as $key => $value)
+                            {{ $value }},
+                         @endforeach
+                     ],
+                 },{
+                     label: 'Resigned',
+                     borderColor: 'gray',
+                     borderWidth: 1,
+                     // bg-color
+                     backgroundColor: [
+                         @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
+                         "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 50%, 40%)",
+                         @endfor
+                     ],
+                     hoverBackgroundColor: [
+                         @for ($hue=0;$hue<count($resigned_positions_short);++$hue)
+                         "hsl(" + {{ $hue*360/count($emp_positions_short)}} + ", 60%, 45%)",
+                          @endfor
+                     ],
+                     data: [
+                         @foreach ($resigned_positions_short as $key => $value)
+                            {{ $value }},
+                         @endforeach
+                     ],
+                 }],
+
          },
      options: {
             legend: {
@@ -80,13 +93,80 @@
     }
     });
 </script>
+@endsection
 
-<h1>Heading Conclusion</h1>
-<p>
-write something.
-    </p>
+@section('table')
+<h2>Resigned People</h2>
+<table class="table table-bordered">
+<thead>
+<tr>
+  <th scope="col">Details</th>
+  <th scope="col">Numbers</th>
+  <th scope="col">Proportions</th>
+</tr>
+</thead>
+<tbody>
+@foreach ($resigned_positions_short as $position => $cnt)
+  <tr>
+<th scope="row">{{ ucfirst($position) }}</th>
+    <td>{{ $cnt }}</td>
+<td>{{ round(($cnt * 100.0 / $all_resigned_cnt), 2, PHP_ROUND_HALF_DOWN) }}%</td>
+  </tr>
+@endforeach
+  <tr>
+  <th scope="row">All</th>
+    <td>{{$all_resigned_cnt}}</td>
+    <td>100%</td>
+  </tr>
+</tbody>
+</table>
+
+<h2>All Employees</h1>
+<table class="table table-bordered">
+<thead>
+<tr>
+  <th scope="col">Labels</th>
+  <th scope="col">Numbers</th>
+  <th scope="col">Proportions</th>
+</tr>
+</thead>
+<tbody>
+@foreach ($emp_positions_short as $position => $cnt)
+  <tr>
+<th scope="row">{{ ucfirst($position) }}</th>
+    <td>{{ $cnt }}</td>
+<td>{{ round(($cnt * 100.0 / $all_emp_cnt), 2, PHP_ROUND_HALF_DOWN) }}%</td>
+  </tr>
+@endforeach
+    <tr>
+<th scope="row">All</th>
+    <td>{{$all_emp_cnt}}</td>
+    <td>100%</td>
+  </tr>
+</tbody>
+</table>
+
+@endsection
+
+@section('study')
+<ul>
+  <li>The proportions of Junior and Senior are over 70%.</li>
+</ul>
+@endsection
+
+@section('discuss')
+<ul>
+<li>Pleeeeeeaaaaaaaaaaaaaaaaaaase add something!!!!!!!!!!!!</li>
+</ul>
+@endsection
+
+@section('content')
+<ul>
+<li>Resigned people who are Junior and Senior often seek for new jobs.</li>
+</ul>
 @endsection
 
     @section('footer')
     (c) 2019 hoge.
     @endsection
+    
