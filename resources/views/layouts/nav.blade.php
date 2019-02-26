8537d7993a290e3f934cd168bd1ce1e532a01c03
syun560 <al18056@shibaura-it.ac.jp>
2019-02-26 01:54:31 +0000

    Add ages page

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -7,6 +7,7 @@
       <li><a href="{{ route('reasons') }}">Reason</a></li>
       <li><a href="{{ route('genders') }}">Gender</a></li>
       <li><a href="{{ route('marital_status') }}">MaritalStatus</a></li>
+      <li><a href="{{ route('ages') }}">Age</a></li>
       <li><a href="{{ route('distances') }}">Distance</a></li>
       <li><a href="{{ route('positions') }}">Position</a></li>
     </ul>
